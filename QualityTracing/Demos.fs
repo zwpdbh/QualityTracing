ec173699abf0bd92a90f3a5290715017868e42db
zwpdbh <hyperion_z@outlook.com>
2023-01-14 13:44:40 +0800

    Add project files.

---
+++ QualityTracing/Demos.fs
@@ -0,0 +1,98 @@
+﻿namespace QualityTracing
+open System
+
+module Demos = 
+    open ThreadAgent
+    open Person
+    open Photo
+    
+    let demoConcurrentlyUpdatePhoto () = 
+        let persons = 
+            seq {
+                for x in [1..3] do 
+                    yield {
+                        Id = "person" + string x 
+                        Name = "zw" + string x 
+                        DepartmentKey = "Dep" + string x 
+                    }
+            }
+            |> Array.ofSeq
+
+
+        let photo =
+            {
+                Id = "1" 
+                Title = None 
+                Content = System.Text.Encoding.ASCII.GetBytes("hello world!")
+                Format = PicFormat.PNG
+                Messages = None
+                CreatAt = DateTime.Now
+            }
+
+        let threadAgent = new ThreadAgent(photo)
+
+        let messages = [
+            Text {
+                Id = "1"
+                Content = "text message 01"
+                Who = persons[0]
+                CreatedAt = DateTime.Now
+                ForAnnotation = None 
+            }
+            Annotate {
+                Id = "2"
+                title = Some "This part01 has problem"
+                Where = Rectangle({x=100; y = 200}, 50, 75)
+                Who = persons[1]
+                CreatedAt = DateTime.Now
+            }
+            Annotate {
+                Id = "3"
+                title = Some "This part02 has problem"
+                Where = Circle({x=200; y = 300}, 10)
+                Who = persons[2]
+                CreatedAt = DateTime.Now            
+            }
+        ]
+
+        let printMessage messages = 
+            messages 
+            |> List.iter (fun x -> 
+                printfn $"{x}"
+            )
+
+        printfn "Concurrently send messages to photo agent"
+        messages
+        |> List.map threadAgent.AddMessage 
+        |> Async.Parallel 
+        |> Async.RunSynchronously
+        |> Array.choose (fun eachResonse -> 
+            match eachResonse with 
+            | Result.Ok response -> 
+                printfn $"\nAdded Message result:\n {printMessage response}"
+                Some response
+            | _ ->
+                None 
+        )
+        |> ignore 
+
+ 
+        printfn "\nView the message associated with a photo"
+        let response = 
+            async {
+                return! threadAgent.ListMessages ()
+            } 
+            |> Async.RunSynchronously
+        match response with 
+        | Result.Ok messagesList -> 
+            match messagesList with 
+            | Some messages -> 
+                printMessage messages
+            | None -> 
+                failwith "No messages"
+        | err ->
+            failwith $"err: {err}"
+
+
+
+        
---
+++ QualityTracing/Models.fs
@@ -0,0 +1,115 @@
+﻿namespace QualityTracing
+
+module Person = 
+    type Company = 
+        {
+            Id: string 
+            Name: string 
+        }
+
+    type Department = 
+        {
+            Id: string 
+            Name: string 
+            Company: Company
+        }
+
+    type Person =
+        {
+            Id: string  
+            Name: string 
+            DepartmentKey: string 
+        }
+
+
+module Photo = 
+    type Point = {x: int; y: int}
+
+    type Area = 
+        | Rectangle of point: Point* width: int * height: int 
+        | Circle of point: Point * radius: int 
+
+    // Represent a scratch area with message associated with them
+    type Annotation = 
+        {
+            Id: string 
+            title: string option
+            Where: Area
+            Who: Person.Person
+            CreatedAt: System.DateTime
+        }
+
+    type AnnotationId = string 
+    type TextMessage = 
+        {
+            Id: string 
+            Content: string
+            Who: Person.Person
+            CreatedAt: System.DateTime
+            ForAnnotation: AnnotationId option 
+        }
+
+    type Message = 
+        | Text of TextMessage 
+        | Annotate of Annotation
+
+    type PicFormat = 
+        | PNG
+        | JPEG
+
+
+    type Photo = 
+        {
+            Id: string 
+            Title: string option 
+            Content: byte array 
+            Format: PicFormat
+            Messages: Message list option  
+            CreatAt: System.DateTime
+        }
+
+// ThreadAgent represent a place people do communication with a context of a photo
+module ThreadAgent = 
+
+    type ThreadMsg = 
+        | AddPhotoMessage of AsyncReplyChannel<Result<Photo.Message list, string>> * Photo.Message
+        | ListPhotoMessages of AsyncReplyChannel<Result<Photo.Message list option, string>>
+        // TBD:: we could add more messages to support other operations
+
+    type ThreadAgent (photo: Photo.Photo) = 
+        let photo = photo 
+        let agent = 
+            MailboxProcessor.Start(fun inbox -> 
+                let rec loop (messages: Photo.Message list option) = 
+                    async {
+                        let! msg = inbox.Receive()
+                        match msg with 
+                        | AddPhotoMessage (chnl, message) -> 
+                            let messages' = 
+                                match messages with 
+                                | None -> 
+                                    chnl.Reply (Result.Ok [message])
+                                    Some [message]
+                                | Some existingMessages -> 
+                                    chnl.Reply (Result.Ok (message::existingMessages))
+                                    Some (message::existingMessages)
+                                    
+                            return! loop(messages')
+                        | ListPhotoMessages chnl -> 
+                            chnl.Reply (Result.Ok messages)
+                            return! loop(messages)
+                    }
+                loop(photo.Messages)        
+            )
+
+        member x.AddMessage (message: Photo.Message) = 
+            async {
+                return! agent.PostAndAsyncReply(fun chnl -> AddPhotoMessage(chnl, message))
+            }
+
+        member x.ListMessages () = 
+            async {
+                return! agent.PostAndAsyncReply(fun chnl -> ListPhotoMessages(chnl))
+            }
+
+
