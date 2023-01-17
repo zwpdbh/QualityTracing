e8be22d18489691f717bd0a1a9cdb851b9e9f3f7
zwpdbh <hyperion_z@outlook.com>
2023-01-17 17:34:16 +0800

    < done

--- CodeAssignment/Problems.fs
+++ CodeAssignment/Problems.fs
@@ -73,6 +73,117 @@ module Problem02 =
 
 
 
+module Problem03 = 
+    let sampleInput01 = 
+        """
+        xxx.com - - [01/Jul/1995:00:12 -0400] "GET /path01/path02/some.GIF HTTP/1.0" 200 0
+        """
+    let sampleInput02 = 
+        """
+        xxx.com - - [01/Jul/1995:00:12 -0400] "GET /path01/path02/some.GIF HTTP/1.0" 304 0
+        """
 
 
+    let (|GetValidForCode200|_|) (input: string) = 
+        let inputSegements = 
+            input.Trim().Split(" ") 
+            |> List.ofArray
+            |> List.rev 
 
+        match inputSegements with 
+        | _::code::content as line ->
+            if (int code) = 200 then 
+                Some content 
+            else 
+                None 
+        | _ -> 
+            None
+
+    let (|GetValidForMethod|_|) (input: string list) = 
+        match input with 
+        | _::filePath::method::_ -> 
+            if method.ToLower().Contains("get") then 
+                Some filePath
+            else 
+                None 
+        | _ -> 
+            None 
+
+    let (|GetGif|_|) (filePath: string) = 
+        let file = 
+            filePath.Trim().Split("/")
+            |> List.ofArray
+            |> List.rev
+            |> List.tryHead
+        
+        match file with 
+        | Some fileName when fileName.ToLower().Contains("gif") -> 
+            Some fileName
+        | _ -> None 
+
+
+    let getGifFileName (input: string) = 
+        match input with 
+        | GetValidForCode200 txt -> 
+            match txt with 
+            | GetValidForMethod filePath ->
+                match filePath with 
+                | GetGif file -> 
+                    Some file 
+                | _ -> 
+                    None
+            | _ ->
+                None
+        | _ -> 
+            None
+
+
+    let getGifFiles (inputs: string list) = 
+        inputs
+        |> List.choose (fun x -> 
+            getGifFileName x
+        )
+        |> List.distinct
+        |> List.rev
+
+    let workingDirectory = System.Environment.CurrentDirectory
+
+    let saveResultToFile (fileName: string) (results: string list) = 
+        let resultFilePath = System.IO.Path.Combine(workingDirectory, fileName)
+
+        System.IO.File.WriteAllLines(resultFilePath, results)
+        results
+        
+    let getInputsFromFile filePath = 
+        let currFilePath = System.IO.Path.Combine(workingDirectory, filePath)
+        
+        System.IO.File.ReadAllLines currFilePath 
+        |> List.ofArray
+        
+    let demo () = 
+        // read the string filename
+        let filename = System.Console.ReadLine()
+       
+        getInputsFromFile filename
+        |> getGifFiles
+        |> saveResultToFile filename
+        |> List.iter (fun x -> 
+            printfn "%A" x
+        )
+
+    let test () = 
+        match sampleInput01 with 
+        | GetValidForCode200 txt -> 
+            printfn "%A" txt
+            match txt with 
+            | GetValidForMethod filePath ->
+                printfn "%A" filePath
+                match filePath with 
+                | GetGif file -> 
+                    printfn "%A" file 
+                | _ -> 
+                    printfn "No gif file"
+            | _ ->
+                printfn "No filePath"
+        | _ -> 
+            printfn "No 200 response"
